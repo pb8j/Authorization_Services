9c4ececadb9610e7872414393c010afcaed43431
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-07 18:29:09 +0530

    Added git user by id in repo

--- src/index.js
+++ src/index.js
@@ -6,23 +6,26 @@ const apiRoutes = require('./routes/index');
 
 // const {User} = require('./models/index');
 // const bcrypt = require('bcrypt');
-
+const UserRepository = require("./repository/user-repository");
 const app = express();
 
 const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/api', apiRoutes);
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
         // const incomingpassword = '123456';
         // const user = await User.findByPk(3);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
         // console.log(response);
     });
-}   
+}
 
 prepareAndStartServer();
