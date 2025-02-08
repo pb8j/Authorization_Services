fbfa220b246ced14ad3463fe5bc9f63723312b42
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-08 11:35:38 +0530

    Added user isAuth function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -62,6 +62,24 @@ class UserService {
             return newJWT;
         } catch (error) {
             console.log("Something went wrong in the sign in process");
+            throw{error}
+        }
+    };
+
+    async isAuthenticated(token){
+        try {
+            const response = this.verifyToken(token);
+            if(!response) {
+                throw {error: 'Invalid token'}
+            }
+            const user = this.userRepository.getById(response.id);
+            if (!user) {
+                throw{error: 'No user with the corresponding token exists'};
+            };
+            return user.id;
+        } catch (error) {
+            console.log("Something went wrong in the sign in process");
+            throw error;
         }
     }
 
